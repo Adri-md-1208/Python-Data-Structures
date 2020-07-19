285d43129a1681d057add28bf897ff4dc4b27a77
Adrian <a.morales.2019@alumnos.urjc.es>
2020-07-19 12:12:06 +0200

    array

--- Linear Structures/array.py
+++ Linear Structures/array.py
@@ -1,53 +1,109 @@
-'''
+"""
+Python-Data-Structures by Adrian Morales Dato
+GitHub : Adri-md-1208
+E-mail : a.morales.2019@alumnos.urjc.es
+Released under a "GPLv3" license
+
+
 Data structure : array
 Description:
-    An array is a static data structure that contains a finite number of elements that are all of the
-    same type. The array type is predefined by Python and we do not have to implement it.
-Operations:
-    insert : add an element at the index position. O(1)
-    delete : remove an element at the index position. O(1)
+    An array is a dynamic data structure that contains a finite number of elements that are all of the
+    same type (numeric only). The array type is predefined by Python and we do not have to implement it.
+Operations and asymptotic complexity:
+    create_void : create an empty array. O(1)
+    is_void : returns if the array have no elements. O(1)
+    insert_element : add an element at the index position. O(1)
+    delete_index : remove the element at the index position. O(1)
+    delete_element : remove the first occurrence of the element. O(n)
     search : search an element and return his index. O(n)
-    show_array : show all the elements of the array. O(n)
     update : change the value of the element at the index position. O(1)
-Complexity:
-    O(1) : insert, delete, update
-    O(n) : search, show_array
-Types supported (more commons):
+    show_array : show all the elements of the array. O(n)
+Types supported (most common):
     b : signed integer of 1 byte
     B : unsigned integer of 1 byte
     i : signed integer of 2 bytes
     I : unsigned integer of 2 bytes
     f : float of 4 bytes
     d : double of 8 bytes
-'''
+"""
 
 from array import *
+
 
 # IMPLEMENTATION OF ARRAY
 
 def create_void(t):
-    '''
-    :param t: type of the elements
+    """
+    :param t: type of the elements (string)
     :return: void array
-    '''
+    """
     return array(t, [])
 
+
+def is_void(a):
+    """
+    :param a: array
+    :return: boolean
+    """
+    if a.buffer_info()[1] == 0:
+        return True
+    return False
+
+
 def insert_element(a, i, e):
+    """
+    :param a: array
+    :param i: index
+    :param e: element
+    :return: void
+    """
     a.insert(i, e)
 
-# show_array
+
+def delete_index(a, i):
+    """
+    :param a: array
+    :param i: integer
+    :return: void
+    """
+    a.pop(i)
+
+
+def delete_element(a, e):
+    """
+    :param a: array
+    :param e: element
+    :return: void
+    """
+    a.remove(e)
+
+
+def search(a, e):
+    """
+    :param a: array
+    :param e: element
+    :return: index of the element or -1 in case of no occurrences
+    """
+    for i in a:
+        if a[i] == e:
+            return i
+        return -1
+
+
+def update(a, i, e):
+    """
+    :param a: array
+    :param i: index
+    :param e: element
+    :return: void
+    """
+    a[i] = e
+
+
 def show_array(a):
+    """
+    :param a: array
+    :return: void (only prints)
+    """
     for i in a:
         print(i)
-
-# Testing the implementation
-
-typeArray = "b"
-i, ii, iii = 0, 1, 2
-a, b, c = 1, 2, 3
-
-insert_element(myArray, i, a)
-insert_element(myArray, ii, b)
-insert_element(myArray, iii, c)
-show_array(myArray)
-
