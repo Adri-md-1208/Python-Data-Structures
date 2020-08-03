f9bf15356ceb18238aafcb7a79f632b424302c42
Adrian <a.morales.2019@alumnos.urjc.es>
2020-08-03 17:34:14 +0200

    recursive binary search

---
+++ Algorithms/Search/BinarySearch_recursive.py
@@ -0,0 +1,42 @@
+"""
+Python-Data-Structures by Adrian Morales Dato
+GitHub : Adri-md-1208
+E-mail : a.morales.2019@alumnos.urjc.es
+Released under a "GPLv3" license
+
+
+Algorithm : binary search
+
+Description:
+    In the binary search algorithm we take a sorted array of elements and start looking for the element at the middle. If it
+    matches with the searched value the algorithm finish. Otherwise, we repeat the method for the left-half of the elements
+    or the right-half depending on the value.
+
+Algorithmic efficiency : O(log n)
+"""
+
+
+# BINARY SEARCH (RECURSIVE)
+
+def binarySearch_recursive(array, index0, indexN, value):
+    """
+    :param array: array of elements
+    :param value: value to search
+    """
+
+    if index0 <= indexN:
+
+        mid = (index0 + indexN) // 2
+
+        if array[mid] == value:
+            return mid
+
+        elif array[mid] < value:
+            return binarySearch_recursive(array, mid+1, indexN, value)
+
+        elif array[mid] > value:
+            return binarySearch_recursive(array, 0, mid-1, value)
+
+    else:
+        return None
+
